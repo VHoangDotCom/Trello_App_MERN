08a97d66710e4ede1fa883c421e3e42ddaaff86b
Nguyen Viet Hoang (APTECH HN) <hoangnvth2010033@fpt.edu.vn>
2022-09-06 15:03:57 +0700

    Combine API get full board

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -15,7 +15,7 @@ import { cloneDeep } from 'lodash'
 
 export const Column = (props) => {
   const { column, onCardDrop, onUpdateColumn } = props
-  const cards =mapOrder(column.cards, column.cardOrder, 'id')
+  const cards =mapOrder(column.cards, column.cardOrder, '_id')
 
   const [showConfirmModal, setShowConfirmModal] = useState(false)
   const toggleShowConfirmModal = () => setShowConfirmModal(!showConfirmModal)
@@ -60,13 +60,13 @@ export const Column = (props) => {
     const newCardToAdd = {
       id: Math.random().toString(36).substring(2, 5), //5 random charactors
       boardId: column.boardId,
-      columnId: column.id,
+      columnId: column._id,
       title: newCardTitle.trim(),
       cover: null
     }
     let newColumn = cloneDeep(column)
     newColumn.cards.push(newCardToAdd)
-    newColumn.cardOrder.push(newCardToAdd.id)
+    newColumn.cardOrder.push(newCardToAdd._id)
 
     onUpdateColumn(newColumn)
     setNewCardTitle('')
@@ -128,7 +128,7 @@ export const Column = (props) => {
           // }}
           // onDropReady={p => console.log('Drop ready: ', p)}
           groupName="viethoang-columns"
-          onDrop={dropResult => onCardDrop(column.id, dropResult) }
+          onDrop={dropResult => onCardDrop(column._id, dropResult) }
           getChildPayload={index => cards[index] }
           dragClass="card-ghost"
           dropClass="card-ghost-drop"
